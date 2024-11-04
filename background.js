d83e78e5b9534efd375622cfeabed61fdec97f0a
yousefnegmeldin <yousefnegm@gmail.com>
2024-11-05 00:17:31 +0200

    feat: added event listener on each button to send file name

--- background.js
+++ background.js
@@ -1,10 +1,31 @@
-chrome.downloads.onDeterminingFilename.addListener((downloadItem, suggest) => {
-  if (downloadItem.filename.startsWith("GIU_") || downloadItem.filename.startsWith("GUC")) {
-    let newName = renameFile(downloadItem.filename);
-    suggest({ filename: newName });
+let customFileName = "";
+chrome.runtime.onMessage.addListener((request) => {
+  if (request.fileName) {
+    customFileName = request.fileName; // received from content.js
   }
 });
 
-const renameFile = (fileToBeRenamed) => {
-  return "testing!";
-};
+chrome.downloads.onDeterminingFilename.addListener((downloadItem, suggest) => {
+  const fileType = downloadItem.filename.endsWith(".pptx")
+    ? ".pptx"
+    : downloadItem.filename.endsWith(".pdf")
+    ? ".pdf"
+    : downloadItem.filename.endsWith(".zip")
+    ? ".zip"
+    : null;
+
+  // exit if the file is neither .pptx, .pdf, .zip
+  if (!fileType) return true;
+
+  if (downloadItem.filename.startsWith("GIU_") || downloadItem.filename.startsWith("GUC_")) {
+    const newName = customFileName ? `${customFileName}${fileType}` : downloadItem.filename;
+
+    if (newName) {
+      suggest({ filename: newName });
+      customFileName = "";
+      return true;
+    } else {
+      console.error("Invalid filename: empty or undefined.");
+    }
+  }
+});
---
+++ content.js
@@ -0,0 +1,14 @@
+const courseName = document.querySelector("#ContentPlaceHolderright_ContentPlaceHoldercontent_LabelCourseName").innerText;
+const trimmedCourseName = courseName.replace(/\s*\(.*?\)\s*$/, "");
+
+const downloadButtons = document.querySelectorAll("#download");
+
+downloadButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const wholeCard = button.parentElement.parentElement.parentElement;
+    //replace part uses regex to clean name
+    let strongElement = wholeCard.getElementsByTagName("strong")[0].innerText.replace(/^\d+\s*-\s*/, "");
+    const fileName = trimmedCourseName + " - " + strongElement;
+    chrome.runtime.sendMessage({ fileName: fileName });
+  });
+});
