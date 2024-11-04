124c3ba261c5f9fd136852670bfe6380dabadcdc
yousefnegmeldin <yousefnegm@gmail.com>
2024-11-04 22:03:37 +0200

    added rename functionality

--- background.js
+++ background.js
@@ -1,9 +1,10 @@
 chrome.downloads.onDeterminingFilename.addListener((downloadItem, suggest) => {
   if (downloadItem.filename.startsWith("GIU_") || downloadItem.filename.startsWith("GUC")) {
-    // Rename logic: extract or replace based on your needs
     let newName = renameFile(downloadItem.filename);
-
-    // Suggest the new filename
     suggest({ filename: newName });
   }
 });
+
+const renameFile = (fileToBeRenamed) => {
+  return "testing!";
+};
